dbf0cd6adaa78d35b5c5bffe25704447332b67fb
tk <trishank01@gmail.com>
2023-01-21 22:04:14 +0530

    core functionality added

--- src/index.js
+++ src/index.js
@@ -8,8 +8,42 @@
     const employeesList = document.querySelector(".employees__names--list");
     const employeeInfo = document.querySelector(".employees__single--info");
   
-    // Add employee login
+    // Add employee logic
+
+    const createEmployee = document.querySelector(".createEmployee");
+    const addEmployeeModal = document.querySelector(".addEmployee");
+    const addEmployeeForm = document.querySelector(".addEmployee_create");
   
+    createEmployee.addEventListener("click" , () => {
+        addEmployeeModal.style.display = "flex"
+    })
+   
+    addEmployeeModal.addEventListener("click" , (e) => {
+        if(e.target.className === "addEmployee"){
+            addEmployeeModal.style.display = "none"
+        }
+    })
+
+    const dobInput = document.querySelector(".addEmployee-create--dob")
+    dobInput.max = `${new Date().getFullYear() - 18}-${new Date().toISOString().slice(5, 10)}` 
+
+    addEmployeeForm.addEventListener("submit" , (e) => {
+        e.preventDefault()
+        const formData = new FormData(addEmployeeForm)
+        const values = [...formData.entries()]
+        let empData = {}
+        values.forEach((val) => {
+            empData[val[0]] = val[1]
+        })
+        empData.id = employees[employees.length - 1].id + 1;
+        empData.age = new Date().getFullYear() - parseInt(empData.dob.slice(0 , 4) , 10)
+        empData.imageUrl = empData.imageUrl || "https://cdn-icons-png.flaticon.com/512/0/93.png"
+        employees.push(empData)
+        renderEmplyees()
+        addEmployeeForm.reset()
+        addEmployeeModal.style.display = "none"
+    })
+
     // select employee logic
     employeesList.addEventListener("click", (e) => {
       if (e.target.tagName === "SPAN" && selectedEmployeeId !== e.target.id) {
@@ -18,6 +52,17 @@
         renderSingleEmployee();
         //render single employee
       }
+
+      if(e.target.tagName === "I"){
+        employees = employees.filter(emp => String(emp.id) !== e.target.parentNode.id)
+      }
+      if(String(selectedEmployee) === e.target.parentNode.id){
+        selectedEmployeeId = employees[0]?.id || -1;
+        selectedEmployee = employees[0] || {};
+        renderSingleEmployee();
+      }
+
+      renderEmplyees();
     });
   
     const renderEmplyees = () => {
@@ -38,7 +83,12 @@
     //Render Single Employee
   
     const renderSingleEmployee = () => {
-      //deleting employee
+       if(selectedEmployeeId === -1){
+        employeeInfo.innerHTML = ""
+        return
+       }
+
+   
       employeeInfo.innerHTML = `<img src="${selectedEmployee.imageUrl}"/>
        <span class="employees__single--heading">
        ${selectedEmployee.firstName} ${selectedEmployee.lastName} (${selectedEmployee.age})
